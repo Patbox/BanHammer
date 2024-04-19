df70c4e96bcc0d5ea9fd98f121863e5292df704e
Drex <nicknamedrex@gmail.com>
2024-04-20 01:59:27 +0200

    Use proper wrapper lookup

--- src/main/java/eu/pb4/banhammer/impl/database/AbstractSQLDatabase.java
+++ src/main/java/eu/pb4/banhammer/impl/database/AbstractSQLDatabase.java
@@ -4,12 +4,13 @@ import com.google.common.net.InetAddresses;
 import eu.pb4.banhammer.impl.config.ConfigManager;
 import eu.pb4.banhammer.api.PunishmentData;
 import eu.pb4.banhammer.api.PunishmentType;
-import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.text.Text;
 
 import java.sql.*;
 import java.util.UUID;
 import java.util.function.Consumer;
+
+import static eu.pb4.banhammer.impl.BanHammerImpl.SERVER;
 
 
 public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
@@ -45,9 +46,9 @@ public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
             prepStmt.setString(1, punishment.playerUUID.toString());
             prepStmt.setString(2, punishment.playerIP);
             prepStmt.setString(3, punishment.playerName);
-            prepStmt.setString(4, Text.Serialization.toJsonString(punishment.playerDisplayName, DynamicRegistryManager.EMPTY));
+            prepStmt.setString(4, Text.Serialization.toJsonString(punishment.playerDisplayName, SERVER.getRegistryManager()));
             prepStmt.setString(5, punishment.adminUUID.toString());
-            prepStmt.setString(6, Text.Serialization.toJsonString(punishment.adminDisplayName, DynamicRegistryManager.EMPTY));
+            prepStmt.setString(6, Text.Serialization.toJsonString(punishment.adminDisplayName, SERVER.getRegistryManager()));
             prepStmt.setLong(7, punishment.time);
             prepStmt.setLong(8, punishment.duration);
             prepStmt.setString(9, punishment.reason);
@@ -73,9 +74,9 @@ public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
             prepStmt.setString(1, punishment.playerUUID.toString());
             prepStmt.setString(2, punishment.playerIP);
             prepStmt.setString(3, punishment.playerName);
-            prepStmt.setString(4, Text.Serialization.toJsonString(punishment.playerDisplayName, DynamicRegistryManager.EMPTY));
+            prepStmt.setString(4, Text.Serialization.toJsonString(punishment.playerDisplayName, SERVER.getRegistryManager()));
             prepStmt.setString(5, punishment.adminUUID.toString());
-            prepStmt.setString(6, Text.Serialization.toJsonString(punishment.adminDisplayName, DynamicRegistryManager.EMPTY));
+            prepStmt.setString(6, Text.Serialization.toJsonString(punishment.adminDisplayName, SERVER.getRegistryManager()));
             prepStmt.setLong(7, punishment.time);
             prepStmt.setLong(8, punishment.duration);
             prepStmt.setString(9, punishment.reason);
@@ -100,10 +101,10 @@ public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
                         result.getLong("id"),
                         UUID.fromString(result.getString("bannedUUID")),
                         result.getString("bannedIP"),
-                        Text.Serialization.fromJson(result.getString("bannedDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("bannedDisplay"), SERVER.getRegistryManager()),
                         result.getString("bannedName"),
                         UUID.fromString(result.getString("adminUUID")),
-                        Text.Serialization.fromJson(result.getString("adminDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("adminDisplay"), SERVER.getRegistryManager()),
                         result.getLong("time"),
                         result.getLong("duration"),
                         result.getString("reason"),
@@ -125,10 +126,10 @@ public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
                 consumer.accept(new PunishmentData(
                         UUID.fromString(result.getString("bannedUUID")),
                         result.getString("bannedIP"),
-                        Text.Serialization.fromJson(result.getString("bannedDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("bannedDisplay"), SERVER.getRegistryManager()),
                         result.getString("bannedName"),
                         UUID.fromString(result.getString("adminUUID")),
-                        Text.Serialization.fromJson(result.getString("adminDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("adminDisplay"), SERVER.getRegistryManager()),
                         result.getLong("time"),
                         result.getLong("duration"),
                         result.getString("reason"),
@@ -151,10 +152,10 @@ public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
                 consumer.accept(new PunishmentData(
                         UUID.fromString(result.getString("bannedUUID")),
                         result.getString("bannedIP"),
-                        Text.Serialization.fromJson(result.getString("bannedDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("bannedDisplay"), SERVER.getRegistryManager()),
                         result.getString("bannedName"),
                         UUID.fromString(result.getString("adminUUID")),
-                        Text.Serialization.fromJson(result.getString("adminDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("adminDisplay"), SERVER.getRegistryManager()),
                         result.getLong("time"),
                         result.getLong("duration"),
                         result.getString("reason"),
@@ -177,10 +178,10 @@ public abstract class AbstractSQLDatabase implements DatabaseHandlerInterface {
                         result.getLong("id"),
                         UUID.fromString(result.getString("bannedUUID")),
                         result.getString("bannedIP"),
-                        Text.Serialization.fromJson(result.getString("bannedDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("bannedDisplay"), SERVER.getRegistryManager()),
                         result.getString("bannedName"),
                         UUID.fromString(result.getString("adminUUID")),
-                        Text.Serialization.fromJson(result.getString("adminDisplay"), DynamicRegistryManager.EMPTY),
+                        Text.Serialization.fromJson(result.getString("adminDisplay"), SERVER.getRegistryManager()),
                         result.getLong("time"),
                         result.getLong("duration"),
                         result.getString("reason"),
